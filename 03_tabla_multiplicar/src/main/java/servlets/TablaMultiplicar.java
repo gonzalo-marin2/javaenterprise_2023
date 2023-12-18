3393bf684984ea05f13d33e1ce13a36b75d252f1
tarde <tarde@A13E34>
2023-12-18 19:10:45 +0100

    commit proyectos 1 a 3

---
+++ 03_tabla_multiplicar/src/main/java/servlets/TablaMultiplicar.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+//Construir tabla de multiplicar del 1 al 10
+
+public class TablaMultiplicar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		out.println("<html><body><center><table border='1'>");
+		for(int i=1;i<=10;i++) {
+			out.println("<tr><th>"+i);
+			for(int k=1;k<=10;k++) {
+				out.println("<td> "+i*k+" </td>");
+			}
+			out.println("</tr>");
+		}
+		
+		out.println("</table></center></body></html>");
+		out.close();
+		
+	}
+
+}
