022ec0bc70485c073fa27ae035579eed47e657a8
tarde <tarde@A13E34>
2024-01-23 16:40:07 +0100

    commit proyecto 41

---
+++ 41_libreria_v2_carrito/src/main/java/controller/AgregarController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Libro;
+import service.LibrosService;
+
+@WebServlet("/AgregarController")
+public class AgregarController extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int isbn=Integer.parseInt(request.getParameter("isbn"));
+		HttpSession session=request.getSession();
+		LibrosService service=new LibrosService();
+		//recogemos el libro cuyo isbn se recibe como parámetro
+		Libro libro=service.getLibro(isbn);
+		List<Libro> carrito=new ArrayList<>();
+		if(session.getAttribute("carrito")!=null) {
+			carrito=(List<Libro>)session.getAttribute("carrito");
+		}
+		carrito.add(libro);
+		session.setAttribute("carrito", carrito);
+		//envía como respuesta un json con los datos del carrito
+		Gson gson=new Gson();
+		PrintWriter out=response.getWriter();
+		response.setContentType("application/json");
+		out.println(gson.toJson(carrito));
+		//donde estás
+	}
+
+}
